fd97e3c3c94fc12627c109cacfc1d69c072a5494
kiu_tsertsvadze.nato <tsertsvadze.nato@kiu.edu.ge>
2023-02-11 22:10:43 +0400

    added mobile responsive functionality

--- bpms_llc/src/components/Home.js
+++ bpms_llc/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
                   <div className="site-name">bpms.ge</div>
                   <div className="full-name">BUSINESS PROCESS MANAGEMENT SYSTEM</div>
                   <div className="alert">The website is under construction</div>
-                  <div className="info">mailbpms@gmail.com  |  Georgia (+995)  577171020 </div>
+                  <div className="info">mailbpms@gmail.com  |   Georgia (+995)  577171020 </div>
              </div>
         </div>
      );
